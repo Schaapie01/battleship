6574a701860c12886d9056eb3c9b430d2607d377
Schaapie01 <yodaatje03@gmail.com>
2026-03-10 23:20:49 +0100

    Added very basic functionality for placing something on the playing field. -->Error handling yet to be added -->No input validation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub struct playingfield {
+    size: usize,
     field: Vec<u8>,
 }
 pub struct artist {
@@ -7,12 +8,23 @@ pub struct artist {
 
 impl playingfield {
     pub fn new(size: usize) -> playingfield {
-        return playingfield { field: vec![0; (size * size)] };
+        return playingfield { field: vec![0; (size * size)], size };
     }
     pub fn get_playfield(&self) -> Vec<u8> {
         return self.field.clone();
     }
-
+    pub fn place(&mut self, player: u8, row: u8, col: u8) {
+        let column = row * (self.size as u8) + col;
+        if self.get_playfield()[column as usize] == 0 {
+            match player {
+                1 => {
+                    self.field[column as usize] = 1;
+                },
+                2 => self.field[column as usize] = 2,
+                _ => panic!("Invalid player")
+            }
+        }
+    }
 }
 
 pub fn draw_field(playfield: Vec<u8>, size: usize) {
--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,22 @@ use crate::lib::draw_field;
 const SIZE: usize = 3;
 
 fn main() {
-    let field  = playingfield::new(SIZE);
+    let mut field  = playingfield::new(SIZE);
     draw_field(field.get_playfield(), SIZE);
+
+    println!("Enter the row number");
+    let mut row = String::new();
+    io::stdin().read_line(&mut row).expect("failed  input");
+
+    println!("Enter the column number");
+    let mut col = String::new();
+    io::stdin().read_line(&mut col).expect("failed  input");
+    row = row.trim().to_string();
+    col=col.trim().to_string();
+    println!("You entered {row}, {col}");
+    field.place(1, row.trim().parse::<u8>().unwrap(), col.trim().parse::<u8>().unwrap());
+    draw_field(field.get_playfield(), SIZE);
+
 
     // let socket = UdpSocket::bind("127.0.0.1:7878").expect("couldn't bind to address");
     // socket.connect("127.0.0.1:34253").unwrap();

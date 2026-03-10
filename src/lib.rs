d7407d8ecf6cdad84b743cd74686f0aa47175aa9
Schaapie01 <yodaatje03@gmail.com>
2026-03-10 19:51:37 +0100

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub struct playingfield {
     field: Vec<u8>,
 }
+pub struct artist {
+    size: usize,
+}
 
 impl playingfield {
     pub fn new(size: usize) -> playingfield {
@@ -8,5 +11,21 @@ impl playingfield {
     }
     pub fn get_playfield(&self) -> Vec<u8> {
         return self.field.clone();
+    }
+
+}
+
+pub fn draw_field(playfield: Vec<u8>, size: usize) {
+    let mut spot_counter = 0;
+    while spot_counter < playfield.len() {
+        match playfield[spot_counter] {
+            1 => print!("[X]"),
+            2 => print!("[O]"),
+            _ => print!("[_]"),
+        }
+        if (spot_counter + 1) % size == 0 {
+            print!("\n");
+        }
+        spot_counter += 1;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,17 @@ pub mod lib;
 
 use std::net::UdpSocket;
 use std::io;
-use battleship::playingfield;
+use battleship::{
+    playingfield,
+    artist
+};
+
+use crate::lib::draw_field;
 const SIZE: usize = 3;
 
 fn main() {
-    let mut field  = playingfield::new(SIZE);
-
-    let mut spot_counter = 0;
-    while spot_counter < field.get_playfield().len() {
-        match field.get_playfield()[spot_counter] {
-            1 => print!("[X]"),
-            2 => print!("[O]"),
-            _ => print!("[_]"),
-        }
-        if (spot_counter + 1) % SIZE == 0 {
-            print!("\n");
-        }
-        spot_counter += 1;
-    }
+    let field  = playingfield::new(SIZE);
+    draw_field(field.get_playfield(), SIZE);
 
     // let socket = UdpSocket::bind("127.0.0.1:7878").expect("couldn't bind to address");
     // socket.connect("127.0.0.1:34253").unwrap();
